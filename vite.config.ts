54890fde39ae529106573bb0419ed9e0df611407
sgtho <sgtho@liverpool.ac.uk>
2025-08-22 00:50:28 +0800

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -16,6 +16,7 @@ export default defineConfig(({ mode }) => ({
     },
   },
   build: {
+    sourcemap: true,
     // Optimize CSS handling for better performance
     cssCodeSplit: true,
     rollupOptions: {
