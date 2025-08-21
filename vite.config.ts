722858b6026ded93450d510e1197e62d6d759da7
sgtho <sgtho@liverpool.ac.uk>
2025-08-21 22:28:19 +0800

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -4,11 +4,10 @@ import path from "path";
 import { componentTagger } from "lovable-tagger";
 
 // https://vitejs.dev/config/
-export default defineConfig(({ mode }) => ({
-  server: {
-    host: "::",
-    port: 8080,
-  },
+export default defineConfig({
+  base: '/Dafan-Health-Official-Website/',
+  // ...add your plugins and other config options here
+}),
   plugins: [
     react(),
     mode === 'development' &&
